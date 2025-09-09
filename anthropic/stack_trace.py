e2667c0ebef70489910819dab313c0cc5a9d6f58
Qibin Chen <qchen35@apple.com>
2025-09-08 17:55:01 -0700

    update

--- anthropic/stack_trace.py
+++ anthropic/stack_trace.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Tuple
+from collections import defaultdict
 
 
 @dataclass
@@ -8,33 +9,43 @@ class Sample:
     stack: List[str]  # outermost -> innermost
 
 
-def samples_to_trace(samples: List[Sample]) -> List[Tuple[float, str, str]]:
+def samples_to_trace(samples: List[Sample], N: int) -> List[Tuple[float, str, str]]:
     if not samples: return []
 
     results = []
-
-    def _start_trace(t: float, substack: List[str]):
-        for name in substack:
-            results.append((t, "s", name))
-            
-    def _end_trace(t: float, substack: List[str]):
-        for name in substack[::-1]:
-            results.append((t, "e", name))
-
-    _start_trace(samples[0].t, samples[0].stack)
+    counts = defaultdict(int)
+    for pos, name in enumerate(samples[0].stack):
+        counts[(pos, name)] += 1
+        if counts[pos, name] == N:
+            results.append((samples[0].t, "s", name))
 
     for idx in range(1, len(samples)):
         prev_sample = samples[idx - 1]
         sample = samples[idx]
+        t = sample.t
+
+        # match prefix traces
         pos = 0
         for a, b in zip(prev_sample.stack, sample.stack):
             if a == b:
+                counts[pos, b] += 1
+                if counts[pos, b] == N:
+                    results.append((t, "s", b))
                 pos += 1
             else:
                 break
 
-        _end_trace(sample.t, prev_sample.stack[pos:])
-        _start_trace(sample.t, sample.stack[pos:])
+        for c_pos, c in list(counts.keys()):
+            if c_pos >= pos:
+                if counts[c_pos, c] >= N:
+                    results.append((t, "e", c))
+                counts.pop((c_pos, c))
+
+        # add new traces
+        for new_pos in range(pos, len(sample.stack)):
+            counts[new_pos, sample.stack[new_pos]] = 1
+            if counts[new_pos, sample.stack[new_pos]] == N:
+                results.append((sample.t, "s", sample.stack[new_pos]))
 
     return results
 
@@ -44,8 +55,16 @@ if __name__ == "__main__":
         Sample(1.0, ['a','a','b','c']),
         Sample(2.0, ['a','a','b','c']),
         Sample(3.0, ['a','a','b','c']),
-        Sample(3.0, ['a',]),
-        # Sample(2.0, ['a','a','b','c', "e"]),
+        Sample(4.0, ['a',]),
+        Sample(5.0, ['a','a','b','c', "e"]),
     ]
+    # (2.0, 's', 'a')
+    # (3.0, 's', 'a')
+    # (3.0, 's', 'b')
+    # (3.0, 's', 'c')
+    # (4.0, 'e', 'a')
+    # (4.0, 'e', 'b')
+    # (4.0, 'e', 'c')
 
-    print(samples_to_trace(s2))
+    for trace in samples_to_trace(s2, N=1):
+        print(trace)
